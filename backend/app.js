37d1776059aa1b1af7689e81041729594c75bed6
Timofey Voronov <timmycarousel@gmail.com>
2023-07-25 23:45:00 +0300

    пр15 поправить корс в бэке проба

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(cors);
 
 // Подключение к серверу MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/mestodb");
+mongoose.connect("mongodb://localhost:27017/mestodb");
 
 app.use(requestLogger);
 
