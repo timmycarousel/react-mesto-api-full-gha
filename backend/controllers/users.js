f3f4f1d2581df3bff3a42ffe36b0179b0a8602ff
Timofey Voronov <timmycarousel@gmail.com>
2023-07-27 11:14:58 +0300

    пр15 ищем токен в бэке

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -78,7 +78,8 @@ const login = (req, res, next) => {
 
           console.log('корректный пароль');
           console.log(`"токен"${token}`);
-          console.log('Заголовок авторизации:', req.headers.authorization);
+          console.log('Заголовок авторизации:', req.cookies.authorization);
+          console.log('Заголовок авторизации:', req.cookie);
 
           return res.status(200).send({ token });
         });
